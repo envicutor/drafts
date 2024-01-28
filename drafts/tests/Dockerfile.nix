9412520ae6195996b09698b99b95c2429fe9cde0
Omar Brikaa <brikaaomar@gmail.com>
2024-01-28 22:39:23 +0200

    Nix experiments

--- drafts/tests/Dockerfile.nix
+++ drafts/tests/Dockerfile.nix
@@ -1,10 +1,5 @@
 FROM nixos/nix
 
-COPY shell.nix cutor.nix .
-
-# Warm up nix-shell
-RUN nix-shell --command "sleep 0" && rm -rf shell.nix cutor.nix
-
 # install nsjail (/root/.nix-profile/bin/nsjail)
 RUN nix-channel --verbose --update && nix-env -iA nixpkgs.nsjail
 
--- drafts/tests/shell.nix
+++ drafts/tests/shell.nix
@@ -1,43 +1,52 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4fddc9be4eaf195d631333908f2a454b03628ee5.tar.gz") {} }:
 let
-  cln-m1 = pkgs.stdenv.mkDerivation {    # this line causes an error
-    name = "cln-m1x";
-    src = pkgs.fetchurl {
-      url = "https://www.gsinac.de/CLN/cln-1.3.6.tar.bz2";
-    };    # I also want to pass some flags to "configure" here
-  };
-  cln-m2 = pkgs.stdenv.mkDerivation {    # this line causes an error
-    name = "cln-m2x";
-    src = pkgs.fetchurl {
-      url = "https://www.gsinac.de/CLN/cln-1.3.6.tar.bz2";
-    };    # I also want to pass some flags to "configure" here
-  };
-  cln-m3 = pkgs.stdenv.mkDerivation {    # this line causes an error
-    name = "cln-m3";
-    src = pkgs.fetchurl {
-      url = "https://www.gssinac.de/CLN/cln-1.3.6.tar.bz2";
-    };    # I also want to pass some flags to "configure" here
-  };
-  cln-m4 = pkgs.stdenv.mkDerivation {    # this line causes an error
-    name = "cln-m4";
-    src = pkgs.fetchurl {
-      url = "https://www.gsssinac.de/CLN/cln-1.3.6.tar.bz2";
-    };    # I also want to pass some flags to "configure" here
-  };
-  cln-m5 = pkgs.stdenv.mkDerivation {    # this line causes an error
-    name = "cln-m5";
-    src = pkgs.fetchurl {
-      url = "https://www.gsssinacj.de/CLN/cln-1.3.6.tar.bz2";
-    };    # I also want to pass some flags to "configure" here
-  };
-  cln-m6 = pkgs.stdenv.mkDerivation {    # this line causes an error
-    name = "cln-m6";
-    src = pkgs.fetchurl {
-      url = "https://www.gssssinacj.de/CLN/cln-1.3.6.tar.bz2";
-    };    # I also want to pass some flags to "configure" here
-  };
+  # cln-m1 = pkgs.stdenv.mkDerivation {    # this line causes an error
+  #   name = "cln-m1xx";
+  #   src = pkgs.fetchurl {
+  #     url = "https://www.gsinac.de/CLN/cln-aslkdj1.3.6.tar.bz2";
+  #   };    # I also want to pass some flags to "configure" here
+  # };
+  # cln-m2 = pkgs.stdenv.mkDerivation {    # this line causes an error
+  #   name = "cln-m2xx";
+  #   src = pkgs.fetchurl {
+  #     url = "https://asldkjwww.gsinac.de/CLN/cln-1.3.6.tar.bz2";
+  #   };    # I also want to pass some flags to "configure" here
+  # };
+  # cln-m3 = pkgs.stdenv.mkDerivation {    # this line causes an error
+  #   name = "cln-m3x";
+  #   src = pkgs.fetchurl {
+  #     url = "https://www.gssinaslkdjac.de/CLN/cln-1.3.6.tar.bz2";
+  #   };    # I also want to pass some flags to "configure" here
+  # };
+  # cln-m4 = pkgs.stdenv.mkDerivation {    # this line causes an error
+  #   name = "cln-m4x";
+  #   src = pkgs.fetchurl {
+  #     url = "https://www.gsssinac.deasdlkj/CLN/cln-1.3.6.tar.bz2";
+  #   };    # I also want to pass some flags to "configure" here
+  # };
+  # cln-m5 = pkgs.stdenv.mkDerivation {    # this line causes an error
+  #   name = "cln-m5x";
+  #   src = pkgs.fetchurl {
+  #     url = "https://www.gsssinaasdlkjcj.de/CLN/cln-1.3.6.tar.bz2";
+  #   };    # I also want to pass some flags to "configure" here
+  # };
+  # cln-m6 = pkgs.stdenv.mkDerivation {    # this line causes an error
+  #   name = "cln-m6x";
+  #   src = pkgs.fetchurl {
+  #     url = "https://www.gssssinacj.deasldkjaslkj/CLN/cln-1.3.6.tar.bz2";
+  #   };    # I also want to pass some flags to "configure" here
+  # };
+  # compile_script = "
+  # $PATH=${pkgs.gcc}/bin
+  # ${pkgs.rustc}/bin/cargo build
+  # ";
+  # run_script = "${pkgs.rustc}/bin/cargo run";
 in pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
+    # python3
+    # ghc
+    # ruby
+    rustc
     # cln-m1
     # cln-m2
     # cln-m3
