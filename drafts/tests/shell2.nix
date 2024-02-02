ad36219950446f062fecb09099aad6543bd7c91b
Omar Brikaa <brikaaomar@gmail.com>
2024-02-02 21:20:54 +0200

    Experiments, notes

--- drafts/tests/shell2.nix
+++ drafts/tests/shell2.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4fddc9be4eaf195d631333908f2a454b03628ee5.tar.gz") {} }:
+{ pkgs ? import <nixpkgs> {} }:
 let
   # cln-m1 = pkgs.stdenv.mkDerivation {
   #   name = "cln-m1xx";
