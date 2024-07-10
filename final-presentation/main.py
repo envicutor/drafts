a7bb54e69a0d1364a58466d7cad76857fa72048f
Omar Brikaa <brikaaomar@gmail.com>
2024-07-10 21:08:51 +0300

    Final presentation assets

---
+++ final-presentation/main.py
@@ -0,0 +1,3 @@
+x = 0
+for i in range(200000):
+    x += 1
---
+++ final-presentation/shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import (
+  fetchTarball {
+    url="https://github.com/NixOS/nixpkgs/archive/72da83d9515b43550436891f538ff41d68eecc7f.tar.gz";
+    sha256="177sws22nqkvv8am76qmy9knham2adfh3gv7hrjf6492z1mvy02y";
+  }
+) {} }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+  	python3
+  ];
+}
